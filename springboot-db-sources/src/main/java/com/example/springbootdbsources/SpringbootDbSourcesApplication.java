71412cf6631ce0c37aa113044953fc78bf94900a
liheyan <1161643430@qq.com>
2023-08-07 23:35:04 +0800

    1: springboot-db-sources init.

---
+++ springboot-db-sources/src/main/java/com/example/springbootdbsources/SpringbootDbSourcesApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootdbsources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liheyan
+ */
+@SpringBootApplication
+public class SpringbootDbSourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDbSourcesApplication.class, args);
+	}
+
+}
---
+++ springboot-db-sources/src/test/java/com/example/springbootdbsources/SpringbootDbSourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdbsources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDbSourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
